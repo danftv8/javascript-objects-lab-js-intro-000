363214dec071ebf5045456458ce9ce8e243bef22
Daniel Mullaney <danftv@gmail.com>
2021-01-12 22:10:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  delete newObj.[key];
+  delete newObj.key;
   return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key)
 {
-  delete object.[key];
+  delete object.key;
   return object;
 }
